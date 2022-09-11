37dfa26f96ae87a5d4f07f0213883c461db3f8eb
rgsk <guptarahul70322@gmail.com>
2022-09-11 15:04:40 +0530

    debouncing filter input

--- src/components/ColumnFilter.tsx
+++ src/components/ColumnFilter.tsx
@@ -1,15 +1,27 @@
 import { FilterProps, Renderer, UseFiltersColumnProps } from "react-table";
 import { Person } from "./columns";
+import { useAsyncDebounce } from "react-table";
+import { useEffect, useState } from "react";
 
 const ColumnFilter: Renderer<FilterProps<Person>> = ({ column }) => {
   const { filterValue, setFilter } =
     column as unknown as UseFiltersColumnProps<Person>;
+  const [value, setValue] = useState(filterValue);
+  const onChange = useAsyncDebounce((value) => {
+    setFilter(value || undefined);
+  }, 300);
+  //   useEffect(() => {
+  //     console.log({ value, filterValue });
+  //   }, [value, filterValue]);
   return (
     <span>
       Search:{" "}
       <input
-        value={filterValue || ""}
-        onChange={(e) => setFilter(e.target.value)}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
       />
     </span>
   );
