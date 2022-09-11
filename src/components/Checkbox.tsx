254417faba76857a5f864a84fe298be72a5f628b
rgsk <guptarahul70322@gmail.com>
2022-09-11 15:50:28 +0530

    checkbox forwardRef implementation

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,6 +1,19 @@
-const Checkbox = ({ indeterminate, ...props }: any) => {
-  return (
-    <input type="checkbox" indeterminate={String(indeterminate)} {...props} />
-  );
-};
+import { forwardRef, useEffect, useRef } from "react";
+
+const Checkbox = forwardRef<HTMLInputElement>(
+  ({ indeterminate, ...rest }: any, ref) => {
+    const defaultRef = useRef<HTMLInputElement>();
+    const resolvedRef: any = ref || defaultRef;
+    useEffect(() => {
+      if (resolvedRef.current) {
+        resolvedRef.current.indeterminate = indeterminate;
+      }
+    }, [resolvedRef, indeterminate]);
+    return (
+      <>
+        <input type="checkbox" ref={resolvedRef} {...rest} />
+      </>
+    );
+  }
+);
 export default Checkbox;
